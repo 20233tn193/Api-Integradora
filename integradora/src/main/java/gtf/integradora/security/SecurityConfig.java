3ba505f2916673f1e246b255cfa68ff0bb43cf8b
DavidReds7 <20233tn152@utez.edu.mx>
2025-04-04 23:31:44 -0600

    cambios

--- integradora/src/main/java/gtf/integradora/security/SecurityConfig.java
+++ integradora/src/main/java/gtf/integradora/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package gtf.integradora.security;
 
 import java.util.stream.Collectors;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
